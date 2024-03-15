3a5c07caaa4b01fd2c0e92f758fee684ca6fb707
Maggitarius <144776072+Maggitarius@users.noreply.github.com>
2024-03-15 23:46:50 +0300

    Add files via upload

---
+++ FirstPractice2/FirstPractice2/Page1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstPractice2;
+
+namespace FirstPractice1
+{
+    public partial class Page1 : Page
+    {
+        private LibraryDatabaseEntities library = new LibraryDatabaseEntities();
+        public Page1()
+        {
+            InitializeComponent();
+            bebra.ItemsSource = library.Authors.ToList();
+        }
+    }
+}
+
---
+++ FirstPractice2/FirstPractice2/Page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstPractice2;
+
+namespace FirstPractice1
+{
+    public partial class Page2 : Page
+    {
+        private LibraryDatabaseEntities library1 = new LibraryDatabaseEntities();
+        public Page2()
+        {
+            InitializeComponent();
+            biba.ItemsSource = library1.Books.ToList();
+        }
+    }
+}
